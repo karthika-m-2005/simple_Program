1f2ed43e170fd4d94abc7829daca2be32c18f259
KARTHIKA M <km646383@gmail.com>
2025-01-24 21:55:14 +0530

    Create if_else.java

---
+++ if_else.java
@@ -0,0 +1,27 @@
+import java.io.*;
+
+
+public class Solution {
+
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int N = scanner.nextInt();
+        
+        if(N%2!=0)
+        {
+            System.out.println("Weird");
+        }
+        else if((N%2==0 && N>=2 && N<=5) || (N%2==0 && N>20) )
+        {
+            System.out.println("Not Weird");
+        }
+        else if(N%2==0 && N>=6 && N<=20)
+        {
+            System.out.println("Weird");
+        }
+        scanner.close();
+    }
+}
