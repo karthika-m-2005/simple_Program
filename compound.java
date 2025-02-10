baf674298f939d73b6704a0d98d2c53170e3168a
KARTHIKA M <km646383@gmail.com>
2025-02-10 21:18:03 +0530

    Create compound.java

---
+++ compound.java
@@ -0,0 +1,11 @@
+public class compound {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int a = 3,b=4,c;
+		c=a+b+a++ + b++ + ++a + ++b;
+	    System.out.println(c);
+	}
+
+}
