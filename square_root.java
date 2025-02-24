dc7ccf9cc59164311286892d0272ab2e0582b498
KARTHIKA M <km646383@gmail.com>
2025-02-24 18:11:53 +0530

    Square Root Program

---
+++ square_root.java
@@ -0,0 +1,15 @@
+import java.lang.Math;
+import java.util.Scanner;
+
+
+public class PackageDemo {
+
+	public static void main(String[] args) {
+		Scanner sc= new Scanner(System.in);
+		System.out.print("Enter the Number: ");
+		int n = sc.nextInt();
+		System.out.println("Square Root of Number: "+n+Math.sqrt(n));
+
+	}
+
+}
